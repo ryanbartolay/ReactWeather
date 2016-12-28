a0946c28cae8ea9daa8af91d20700cfae118c7ec
Ryan <bartolay.ryan@gmail.com>
2016-12-28 19:48:12 +0800

    Section5-Lesson40-Refactoring-Stateless-Functional-Components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,11 @@
 var React = require("react");
 
-var About = React.createClass({
-    render: function () {
-        return (
+// Refactored component
+// this is essential for stateless component
+var About = (props) => {
+    return (
             <h2>About</h2>
         );
-    }
-});
+};
 
 module.exports = About;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,14 +1,8 @@
 var React = require("react");
 
-var WeatherMessage = React.createClass({
-    render: function () {
-
-        var {location, temp} = this.props; // 
-        
-        return (
-            <h2>It is {temp} in {location}</h2>
-        );
-    }
-});
-
+var WeatherMessage = ({location, temp}) => {
+    return (
+        <h2>It is {temp} in {location}</h2>
+    );
+};
 module.exports = WeatherMessage;
